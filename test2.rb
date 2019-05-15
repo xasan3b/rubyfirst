0d09cf8ade8ba4f67054c449856b14cefae4a2a2
hassan <xasan3b@gmail.com>
2019-05-15 17:09:51 +0800

    ruby fix

---
+++ test2.rb
@@ -0,0 +1,15 @@
+puts "Please enter a number from 0 to a 100"
+number = gets.to_i 
+if number >= 0 && number <= 50 
+    puts "you number is less the 50"
+elsif number >= 51 && number <= 100
+    puts "your number is less than a 100 "
+else
+    puts "number is more than a 100"    
+end
+
+
+
+    
+
+ 
---
+++ test3.rb
@@ -0,0 +1,42 @@
+puts "please enter your name"
+first_name = gets.chomp
+puts "please inter your last name"
+last_name = gets.chomp
+puts"how old are you"
+age = gets.chomp.to_i
+puts "hello #{first_name} #{last_name}"
+name = first_name + " " + last_name
+puts "your age after 10 years will be #{age + 10}"
+puts "your age after 20 years will be #{age + 20}"
+puts "your age after 30 years will be #{age + 30}"
+puts "your age after 40 years will be #{age + 40}"
+if age <= 20
+    puts " you age is under 20"
+
+elsif age >= 21 && age <= 40
+    puts "you are bettween 20 and 40."
+    
+else
+    puts "you are above 40"
+end
+puts "your full name reversed is #{name.reverse}"
+puts "you full name has #{name.length - 1} Chaaractars"
+puts "your full name in uppercase is #{name.upcase}"
+
+#gender
+puts "whats your gender"
+gender = gets.chomp
+
+gender = case
+when gender.downcase == "male" 
+puts " hello sir"
+else 
+    puts "hello madam"
+end
+
+if age == even
+    
+
+
+
+
